19f5e608a9176917b9e339abcd5545cbdeaa2382
shoaibakhtar800 <cs08018akp@gmail.com>
2023-04-16 13:13:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import './styles.scss';
+import { useState } from 'react';
+import Board from './components/Board';
+import StatusMessage from './components/StatusMessage';
+import History from './components/History';
+import { calculateWinner } from './winner';
+
+const NEW_GAME = [{ squares: Array(9).fill(null), isXNext: false }];
+
+function App() {
+  const [history, setHistory] = useState(NEW_GAME);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const gamingBoard = history[currentMove];
+
+  const { winner, winningSquares } = calculateWinner(gamingBoard.squares);
+
+  const handleSquareClick = clickedPosition => {
+    if (gamingBoard.squares[clickedPosition] || winner) {
+      return;
+    }
+
+    setHistory(currentHistory => {
+      const isTraversing = currentMove + 1 !== currentHistory.length;
+
+      const lastGamingState = isTraversing
+        ? currentHistory[currentMove]
+        : history[history.length - 1];
+
+      const nextSquaresState = lastGamingState.squares.map(
+        (squareValue, position) => {
+          if (clickedPosition === position) {
+            return lastGamingState.isXNext ? 'X' : 'O';
+          }
+          return squareValue;
+        }
+      );
+
+      const base = isTraversing
+        ? currentHistory.slice(0, currentHistory.indexOf(lastGamingState) + 1)
+        : currentHistory;
+
+      return base.concat({
+        squares: nextSquaresState,
+        isXNext: !lastGamingState.isXNext,
+      });
+    });
+
+    setCurrentMove(move => move + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
+  const onNewGameStart = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
+  return (
+    <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
+      <StatusMessage winner={winner} gamingBoard={gamingBoard} />
+      <Board
+        squares={gamingBoard.squares}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+
+      <button
+        type="button"
+        onClick={onNewGameStart}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
+        Start new game
+      </button>
+
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current game history
+      </h2>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
+
+      <div className="bg-balls"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,16 @@
+const Square = ({ value, onClick, isWinningSquare }) => {
+    const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+    const winningClassName = isWinningSquare ? 'winning' : '';
+  
+    return (
+      <button
+        type="button"
+        className={`square ${colorClassName} ${winningClassName}`}
+        onClick={onClick}
+      >
+        {value}
+      </button>
+    );
+  };
+  
+  export default Square;
